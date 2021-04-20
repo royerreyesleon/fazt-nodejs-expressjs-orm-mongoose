3d0f5bb7b68534be0db387fc169c07efcb70b667
Royer Reyes León <royer.stecnica@gmail.com>
2021-04-20 05:35:32 -0500

    Agregando cliente que se comunica con la API REST

--- src/server.js
+++ src/server.js
@@ -1,18 +1,23 @@
 const { json } = require('express');
-const express = require('express')
-const path = require('path');
+const express  = require('express')
+const path     = require('path');
 
 // INICIALIZANDO
-const app = express();
+const app      = express();
+
 
 
 // CONFIGURACIONES
-
 // require('dotenv').config()
 // const port = process.env.PORT || 3007
 app.set('port', process.env.PORT || 3007);
+
+
+
+// CLIENTE
 app.set('views', path.join(__dirname, 'views')); // VISTAS
 app.set('view engine', 'ejs'); // ESTABLECER EL MOTOR DE PLANTILLA
+
 
 
 // MIDDLEWARES (FUNCIONES ANTES QUELLEGUEN A LAS RUTAS)
@@ -21,18 +26,23 @@ app.use(express.urlencoded({extended: false})) // MANIPULAR LOS DATOS QUE VENGAN
 // VARIABLES GLOBALES
 
 
-// RUTAS
+
+// RUTAS | CLIENTE
 app.get('/' , (req , res)=>{
    // res.send('hello from simple server :)')
    res.render('index.ejs');
 })
 
+
+
+// RUTAS | API
 app.use(require('./routes/notes.routes'))
+
+
 
 // ARCHIVOS ESTATICOS (PUBLICOS)
 // STATIC (ARCHIVOS ESTATICOS) 
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(express.static(path.join(__dirname, 'views')));
 
 
 module.exports = app;
